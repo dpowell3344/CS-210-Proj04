1a156b4ac8a4f7b449296ac13de4421e77979d43
Deahja <powelld@seawolf.sonoma.edu>
2014-10-30 16:52:53 -0700

    Added a default value

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,25 @@
+// Assignment: Project 4
 // Author: Deahja Powell
 // File: hello.cpp
-// 
+
 #include <iostream>
-
 using namespace std;
-
 
 int main (int argc, char* argv[])
 {
 
-  cout << "Hello, " << argv[10] << endl;
+  // Default is "World"
+  if(argc == 1)
+    {
+      cout << "Hello, " << argv[10] << endl;
+    }
+  else
+    {
+      cout << "Hello World!" << endl;
+    }
+    
+  
+  //cout << "Hello, " << argv[10] << endl;
   return 0;
 
 }
